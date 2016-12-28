e760d57951f975fc3098f50cd1218807cea202d2
Oleksii Markov <malexer@gmail.com>
2016-12-28 23:45:25 +0200

    Added plugin code to set SPARK_HOME based on value in pytest config

---
+++ pytest_spark.py
@@ -0,0 +1,27 @@
+import os
+
+import findspark
+
+
+def update_spark_home(spark_home):
+    """Set SPARK_HOME with provided path and perform all required
+    configuration to make pyspark importable.
+    """
+
+    if spark_home is not None:
+        spark_home = os.path.abspath(spark_home)
+        if not os.path.exists(spark_home):
+            raise OSError(
+                "SPARK_HOME path specified in config does not exist: %s"
+                % spark_home)
+
+    findspark.init(spark_home)
+
+
+def pytest_addoption(parser):
+    parser.addini('spark_home', 'Spark install directory (SPARK_HOME).')
+
+
+def pytest_configure(config):
+    spark_home = config.getini('spark_home') or None
+    update_spark_home(spark_home)
