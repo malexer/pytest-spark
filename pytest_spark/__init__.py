88b58ad5a75143bcf61c1a90e0e9c8ecf8aeb622
Oleksii Markov <alex@markovs.me>
2017-08-03 17:36:01 +0300

    Updated order of spark_home reading: ini then env

--- pytest_spark/__init__.py
+++ pytest_spark/__init__.py
@@ -4,17 +4,21 @@ import findspark
 import pytest
 
 
-def update_spark_home(spark_home):
+def update_spark_home(spark_home, source):
     """Set SPARK_HOME with provided path and perform all required
     configuration to make pyspark importable.
+
+    :param spark_home: path to Apache Spark
+    :param source: name of the source, used for error message in case if
+                   specified path was not found
     """
 
     if spark_home is not None:
         spark_home = os.path.abspath(spark_home)
         if not os.path.exists(spark_home):
             raise OSError(
-                "SPARK_HOME path specified in config does not exist: %s"
-                % spark_home)
+                "SPARK_HOME path specified in %s does not exist: %s"
+                % (source, spark_home))
 
     findspark.init(spark_home)
 
@@ -24,10 +28,15 @@ def pytest_addoption(parser):
 
 
 def pytest_configure(config):
-    spark_home = os.environ.get('SPARK_HOME', config.getini('spark_home'))
+    source = 'config (pytest.ini)'
+    spark_home = config.getini('spark_home')
+
+    if not spark_home:
+        source = 'ENV'
+        spark_home = os.environ.get('SPARK_HOME')
 
     if spark_home:
-        update_spark_home(spark_home)
+        update_spark_home(spark_home, source)
 
 
 @pytest.fixture(scope='session')
