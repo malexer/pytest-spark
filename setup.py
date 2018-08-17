0152b555eb532710fd5bd212bd95134f9342e22f
Oleksii Markov <omarkov@cloudmade.com>
2018-08-17 14:11:14 +0300

    Incremented version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(cwd, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pytest-spark',
-    version='0.4.4',
+    version='0.4.5',
 
     description='pytest plugin to run the tests with support of pyspark.',
     long_description=long_description,
